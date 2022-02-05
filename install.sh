8e5daad1bde9a1db707e4e3274768cf18e72178a
DangDev <dream858vip@gmail.com>
2022-02-05 23:55:52 +0700

    Update and rename pluto.sh to install.sh

---
+++ install.sh
@@ -0,0 +1,5 @@
+curl -L -o parsec.exe https://github.com/DangDev/QEMU/releases/download/Download/parsec-windows.exe
+curl -L -o onedrive.exe https://github.com/DangDev/QEMU/releases/download/Download/OneDriveSetup.exe
+curl -L -o ultraviewer.exe https://github.com/DangDev/QEMU/releases/download/Download/UltraViewer_setup_6.5_vi.exe
+ultraviewer.exe /VERYSILENT
+onedrive.exe /VERYSILENT
--- pluto.sh
+++
@@ -1,5 +0,0 @@
-curl -L -o parsec.exe #link
-curl -L -o onedrive.exe #link
-curl -L -o ultraviewer.exe #link
-ultraviewer.exe /VERYSILENT
-onedrive.exe /VERYSILENT
