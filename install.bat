cf447835e6c27affab577bad4ba8b16160086607
DangDev <dream858vip@gmail.com>
2022-02-07 21:43:02 +0700

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,5 +1,7 @@
 curl -L -o parsec.exe https://github.com/DangDev/QEMU/releases/download/Download/parsec-windows.exe
 curl -L -o onedrive.exe https://github.com/DangDev/QEMU/releases/download/Download/OneDriveSetup.exe
 curl -L -o ultraviewer.exe https://github.com/DangDev/QEMU/releases/download/Download/UltraViewer_setup_6.5_vi.exe
+curl -L -o turnoffIE.ps1 https://raw.githubusercontent.com/DangDev/QEMU/main/turnoffIE.ps1
+turnoffIE.ps1
 ultraviewer.exe /VERYSILENT
 onedrive.exe /VERYSILENT
