d4adf64c7de1d3365914e358ae7d8da4df4be701
DangDev <dream858vip@gmail.com>
2022-04-24 13:49:32 +0700

    Update installaws.sh

--- installaws.sh
+++ installaws.sh
@@ -10,5 +10,50 @@ echo "region = us-east-1" >> ~/.aws/config
 echo "[default]"  > ~/.aws/credentials
 echo 'aws_access_key_id='$AWS_ACCESS_KEY_ID >> ~/.aws/credentials
 echo 'aws_secret_access_key='$AWS_SECRET_ACCESS_KEY >> ~/.aws/credentials
+aws sagemaker create-notebook-instance --notebook-instance-name learn1 --instance-type ml.p3.2xlarge --role-arn $ARN --direct-internet-access Enabled --root-access Enabled --platform-identifier notebook-al2-v1
+aws sagemaker create-notebook-instance --notebook-instance-name learn2 --instance-type ml.g4dn.xlarge --role-arn $ARN --direct-internet-access Enabled --root-access Enabled --platform-identifier notebook-al2-v1
 aws configure set default.region us-east-2
-aws sagemaker create-notebook-instance --notebook-instance-name learn1 --instance-type ml.t2.medium --role-arn $ARN --direct-internet-access Enabled --root-access Enabled --platform-identifier notebook-al2-v1
+aws sagemaker create-notebook-instance --notebook-instance-name learn1 --instance-type ml.p3.2xlarge --role-arn $ARN --direct-internet-access Enabled --root-access Enabled --platform-identifier notebook-al2-v1
+aws sagemaker create-notebook-instance --notebook-instance-name learn2 --instance-type ml.g4dn.xlarge --role-arn $ARN --direct-internet-access Enabled --root-access Enabled --platform-identifier notebook-al2-v1
+aws configure set default.region us-west-1
+aws sagemaker create-notebook-instance --notebook-instance-name learn1 --instance-type ml.p3.2xlarge --role-arn $ARN --direct-internet-access Enabled --root-access Enabled --platform-identifier notebook-al2-v1
+aws sagemaker create-notebook-instance --notebook-instance-name learn2 --instance-type ml.g4dn.xlarge --role-arn $ARN --direct-internet-access Enabled --root-access Enabled --platform-identifier notebook-al2-v1
+aws configure set default.region us-west-2
+aws sagemaker create-notebook-instance --notebook-instance-name learn1 --instance-type ml.p3.2xlarge --role-arn $ARN --direct-internet-access Enabled --root-access Enabled --platform-identifier notebook-al2-v1
+aws sagemaker create-notebook-instance --notebook-instance-name learn2 --instance-type ml.g4dn.xlarge --role-arn $ARN --direct-internet-access Enabled --root-access Enabled --platform-identifier notebook-al2-v1
+aws configure set default.region ca-central-1
+aws sagemaker create-notebook-instance --notebook-instance-name learn1 --instance-type ml.p3.2xlarge --role-arn $ARN --direct-internet-access Enabled --root-access Enabled --platform-identifier notebook-al2-v1
+aws sagemaker create-notebook-instance --notebook-instance-name learn2 --instance-type ml.g4dn.xlarge --role-arn $ARN --direct-internet-access Enabled --root-access Enabled --platform-identifier notebook-al2-v1
+aws configure set default.region eu-north-1
+aws sagemaker create-notebook-instance --notebook-instance-name learn1 --instance-type ml.p3.2xlarge --role-arn $ARN --direct-internet-access Enabled --root-access Enabled --platform-identifier notebook-al2-v1
+aws sagemaker create-notebook-instance --notebook-instance-name learn2 --instance-type ml.g4dn.xlarge --role-arn $ARN --direct-internet-access Enabled --root-access Enabled --platform-identifier notebook-al2-v1
+aws configure set default.region eu-west-3
+aws sagemaker create-notebook-instance --notebook-instance-name learn1 --instance-type ml.p3.2xlarge --role-arn $ARN --direct-internet-access Enabled --root-access Enabled --platform-identifier notebook-al2-v1
+aws sagemaker create-notebook-instance --notebook-instance-name learn2 --instance-type ml.g4dn.xlarge --role-arn $ARN --direct-internet-access Enabled --root-access Enabled --platform-identifier notebook-al2-v1
+aws configure set default.region eu-west-2 
+aws sagemaker create-notebook-instance --notebook-instance-name learn1 --instance-type ml.p3.2xlarge --role-arn $ARN --direct-internet-access Enabled --root-access Enabled --platform-identifier notebook-al2-v1
+aws sagemaker create-notebook-instance --notebook-instance-name learn2 --instance-type ml.g4dn.xlarge --role-arn $ARN --direct-internet-access Enabled --root-access Enabled --platform-identifier notebook-al2-v1
+aws configure set default.region eu-west-1
+aws sagemaker create-notebook-instance --notebook-instance-name learn1 --instance-type ml.p3.2xlarge --role-arn $ARN --direct-internet-access Enabled --root-access Enabled --platform-identifier notebook-al2-v1
+aws sagemaker create-notebook-instance --notebook-instance-name learn2 --instance-type ml.g4dn.xlarge --role-arn $ARN --direct-internet-access Enabled --root-access Enabled --platform-identifier notebook-al2-v1
+aws configure set default.region eu-central-1
+aws sagemaker create-notebook-instance --notebook-instance-name learn1 --instance-type ml.p3.2xlarge --role-arn $ARN --direct-internet-access Enabled --root-access Enabled --platform-identifier notebook-al2-v1
+aws sagemaker create-notebook-instance --notebook-instance-name learn2 --instance-type ml.g4dn.xlarge --role-arn $ARN --direct-internet-access Enabled --root-access Enabled --platform-identifier notebook-al2-v1
+aws configure set default.region eu-south-1 
+aws sagemaker create-notebook-instance --notebook-instance-name learn1 --instance-type ml.p3.2xlarge --role-arn $ARN --direct-internet-access Enabled --root-access Enabled --platform-identifier notebook-al2-v1
+aws sagemaker create-notebook-instance --notebook-instance-name learn2 --instance-type ml.g4dn.xlarge --role-arn $ARN --direct-internet-access Enabled --root-access Enabled --platform-identifier notebook-al2-v1
+aws configure set default.region ap-south-1 
+aws sagemaker create-notebook-instance --notebook-instance-name learn1 --instance-type ml.p3.2xlarge --role-arn $ARN --direct-internet-access Enabled --root-access Enabled --platform-identifier notebook-al2-v1
+aws sagemaker create-notebook-instance --notebook-instance-name learn2 --instance-type ml.g4dn.xlarge --role-arn $ARN --direct-internet-access Enabled --root-access Enabled --platform-identifier notebook-al2-v1
+aws configure set default.region ap-northeast-1
+aws sagemaker create-notebook-instance --notebook-instance-name learn1 --instance-type ml.p3.2xlarge --role-arn $ARN --direct-internet-access Enabled --root-access Enabled --platform-identifier notebook-al2-v1
+aws sagemaker create-notebook-instance --notebook-instance-name learn2 --instance-type ml.g4dn.xlarge --role-arn $ARN --direct-internet-access Enabled --root-access Enabled --platform-identifier notebook-al2-v1
+aws configure set default.region ap-northeast-2
+aws sagemaker create-notebook-instance --notebook-instance-name learn1 --instance-type ml.p3.2xlarge --role-arn $ARN --direct-internet-access Enabled --root-access Enabled --platform-identifier notebook-al2-v1
+aws sagemaker create-notebook-instance --notebook-instance-name learn2 --instance-type ml.g4dn.xlarge --role-arn $ARN --direct-internet-access Enabled --root-access Enabled --platform-identifier notebook-al2-v1
+aws configure set default.region ap-southeast-1
+aws sagemaker create-notebook-instance --notebook-instance-name learn1 --instance-type ml.p3.2xlarge --role-arn $ARN --direct-internet-access Enabled --root-access Enabled --platform-identifier notebook-al2-v1
+aws sagemaker create-notebook-instance --notebook-instance-name learn2 --instance-type ml.g4dn.xlarge --role-arn $ARN --direct-internet-access Enabled --root-access Enabled --platform-identifier notebook-al2-v1
+aws configure set default.region ap-southeast-2 
+aws sagemaker create-notebook-instance --notebook-instance-name learn1 --instance-type ml.p3.2xlarge --role-arn $ARN --direct-internet-access Enabled --root-access Enabled --platform-identifier notebook-al2-v1
+aws sagemaker create-notebook-instance --notebook-instance-name learn2 --instance-type ml.g4dn.xlarge --role-arn $ARN --direct-internet-access Enabled --root-access Enabled --platform-identifier notebook-al2-v1
