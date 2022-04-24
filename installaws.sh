a5bb282a62412450729a76b1c9eb8efcadbeffbb
DangDev <dream858vip@gmail.com>
2022-04-24 13:42:57 +0700

    Update installaws.sh

--- installaws.sh
+++ installaws.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 apt update
-apt install awscli dialog -y
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+./aws/install
+read -p "Enter ARN: " ARN
 mkdir ~/.aws/
 echo "[default]" > ~/.aws/config
 echo "region = us-east-1" >> ~/.aws/config
@@ -8,4 +11,4 @@ echo "[default]"  > ~/.aws/credentials
 echo 'aws_access_key_id='$AWS_ACCESS_KEY_ID >> ~/.aws/credentials
 echo 'aws_secret_access_key='$AWS_SECRET_ACCESS_KEY >> ~/.aws/credentials
 aws configure set default.region us-east-2
-aws sagemaker create-notebook-instance --notebook-instance-name learn1 --instance-type ml.t2.medium --role-arn $ARN --direct-internet-access Enabled --root-access Enabled --platform-identifier al2-v1 
+aws sagemaker create-notebook-instance --notebook-instance-name learn1 --instance-type ml.t2.medium --role-arn $ARN --direct-internet-access Enabled --root-access Enabled --platform-identifier notebook-al2-v1
